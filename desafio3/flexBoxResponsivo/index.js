9558081f9dc9faccd739567ad1c98539c8c508d1
Monique Rodrigues <moniqueoliveira0104@gmail.com>
2022-03-14 00:24:42 -0300

    commit inicial

---
+++ desafio3/flexBoxResponsivo/index.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){  
+      $("img").click(function(){
+      $(".menu").toggle();
+    });
+  });
+
+
+    $(window).resize(function() {    
+        if($(document).innerWidth()<= 375){
+            $(".menu").hide();       
+            //diminui os elementos, esconde se for necessário
+            console.log("é menor ou igual a 375")                    
+          
+        }else{
+            $(".menu").show();
+            console.log("não é 375")
+            // mantém um elemento ou outro
+          
+        }
+    })
+        
+
